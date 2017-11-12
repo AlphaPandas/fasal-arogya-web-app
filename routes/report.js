1b8a821a5a9cd51cd62ad74968a05128f9d9dcba
CaptainTorch <yashsartanpara@gmail.com>
2017-11-12 09:42:38 +0530

    updated report.js

--- routes/report.js
+++ routes/report.js
@@ -28,7 +28,7 @@ router.post('/', uploadImage, function (req, res, next) {
 
         //var finalFilePath = __dirname +'../public' + newName
         //console.log('Path of file:', finalFilePath)
-        var finalFilePath = '/app/' + newName
+        var finalFilePath = '/app/public/images/' + newName
         fs.writeFileSync(finalFilePath, data);
         console.log("filePath", finalFilePath)
         var serverFileName = 'http://hack-seed.herokuapp.com/' + newName
